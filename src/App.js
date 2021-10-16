34029c1e9ce2a778bf449c79e9ca30b64e6213c5
lukostelloe <lukostelloe@gmail.com>
2021-10-16 10:02:51 +0200

    replacing old project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import "./App.css";
 // import Navbar from "./components/Navbar/Navbar";
 import Input from "./components/Input/Input";
-// import Personlist from "./components/PersonList/Personlist";
-// import Personinput from "./components/Personinput/Personinput";
+// import Test from "./components/Test/Test";
 // import JsonThing from "./components/JsonThing/JsonThing";
 
 function App() {
   return (
     <div className="App">
       <Input />
-      {/* <JsonThing /> */}
     </div>
   );
 }
