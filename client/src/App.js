d26dd0aa5e888b1d5acba830c80f20b2b50886e4
lukostelloe <lukostelloe@gmail.com>
2021-10-21 09:10:13 +0200

    working with links

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
 import Example from "./components/Example/Example";
-import Dashboard from "./components/Dashboard/Dashboard";
+import About from "./components/About/About";
+import Shop from "./components/Shop/Shop";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <div>
-          <Navbar />
-          <Route path="/dashboard" component={Dashboard} />
-        </div>
-        <Example />
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/example" component={Example} />
+          <Route path="/about" component={About} />
+          <Route path="/shop" component={Shop} />
+        </Switch>
       </div>
     </Router>
   );
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -7,18 +7,24 @@ function Navbar() {
     <div>
       <div>
         <ul>
-          <li>
-            <a href="https://www.google.ie/">Luke Costelloe</a>
-          </li>
+          <Link to={"/"}>
+            <li>Luke Costelloe</li>
+          </Link>
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+          <Link to="/example">
+            <li>Example</li>
+          </Link>
+          <Link to="/shop">
+            <li>Shop</li>
+          </Link>
           <li>
             <a href="https://www.100daysofcode.com/">100 days of code</a>
           </li>
           <li>
             <a href="https://twitter.com/home">Twitter</a>
           </li>
-          <Link to={"./Dashboard"}>
-            <li>Dashboard</li>
-          </Link>
         </ul>
       </div>
     </div>
