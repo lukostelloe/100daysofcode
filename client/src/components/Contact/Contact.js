48a32dbcb25cc70e7f114b8c66ba39e909ac62b5
lukostelloe <lukostelloe@gmail.com>
2021-10-23 12:08:48 +0200

    contact form started

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -1,7 +1,36 @@
 import React from "react";
+import "./Contact.css";
 
 function Contact() {
-  return <div>Exercises page</div>;
+  return (
+    <div className="contact_page">
+      <h2>Contact</h2>
+      <form className="contact_form">
+        <label for="firstname">First Name</label>
+        <input type="text" placeholder="First Name" />
+        <label for="lastname">Last Name</label>
+        <input type="text" placeholder="Last Name" />
+        <label for="email">email</label>
+        <input type="text" placeholder="email" />
+        <label for="country">Country</label>
+        <select for="country" placeholder="country">
+          <option value="Ireland">Ireland</option>
+          <option value="France">France</option>
+          <option value="UK">UK</option>
+          <option value="United States">United States</option>
+        </select>
+        <label for="subject">Subject</label>
+        <textarea
+          className="contact_textarea"
+          id="subject"
+          name="subject"
+          placeholder="Write something"
+          maxlength="500"
+        ></textarea>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
 
 export default Contact;
