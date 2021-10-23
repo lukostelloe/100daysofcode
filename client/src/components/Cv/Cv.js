52b30ef4a76646677f6d6b3e7e66925b530cf98a
lukostelloe <lukostelloe@gmail.com>
2021-10-23 15:42:10 +0200

    styling homepage

--- client/src/components/Cv/Cv.js
+++ client/src/components/Cv/Cv.js
@@ -1,32 +1,36 @@
 import React from "react";
 import "./Cv.css";
-import mountain from "./Cvimages/mountain.jpg";
 
 function Cv() {
   return (
-    <div>
-      <div className="head_text">
-        <div>
-          <img src={mountain} alt="mountain" className="mountain" />
-        </div>
-        <div class="text_on_image">
-          <h1> Luke Costelloe </h1>
-          <h2> Junior Web Developer </h2>
-        </div>
-      </div>
-      <div className="about">
+    <div className="cv_page">
+      <div className="cv_page_header">
+        <h1> Luke Costelloe </h1>
         <h2>Welcome to my page!</h2>
         <p>
           I'm a junior developer in school and working part time in web
           development.
         </p>
+      </div>
+      <div className="about">
         <div>
           <h2>Experience</h2>
-          <p>Jan-May 21: Self taught developt, HTML, CSS, JS</p>
-          <p>May-December 21: Web/ Mobile Dev Course.</p>
-          <p>
-            September-Present 21P: Monist Studio. Junior Front End Web Developer
-          </p>
+          <div className="experience_tabs">
+            <div className="exp_1">
+              <p>Jan-May 21</p>
+              <p>Self taught developer</p>
+              <p>HTML, CSS, JS</p>
+            </div>
+            <div className="exp_2">
+              <p>May-December 21</p>
+              <p>Web/ Mobile Dev Course.</p>
+            </div>
+            <div className="exp_3">
+              <p>September-Present 21</p>
+              <p>Monist Studio</p>
+              <p>Junior Front End Web Developer</p>
+            </div>
+          </div>
           <h2>Technologies</h2>
           <p>HTML, CSS, JS, TS, React, PHP, Wordpress, MongoDB, MySQL</p>
         </div>
