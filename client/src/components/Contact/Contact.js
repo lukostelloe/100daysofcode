d23e1f14452013f31c5d5575d7e04e52e75f4faa
lukostelloe <lukostelloe@gmail.com>
2021-10-25 08:55:06 +0200

    emailjs and contact form

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -1,7 +1,36 @@
 import React from "react";
+import emailjs from "emailjs-com";
+import Swal from "sweetalert2";
 import "./Contact.css";
 
+//I NEED TO PUT THESE IN A SECURE ENV FILE
+const SERVICE_ID = "service_9py45ch";
+const TEMPLATE_ID = "template_d5ic7px";
+const USER_ID = "user_iBfg6gc7k8Y9C7UjJiN5Z";
+
 function Contact() {
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID).then(
+      (result) => {
+        console.log(result.text);
+        Swal.fire({
+          icon: "success",
+          title: "Message Sent Successfully",
+        });
+      },
+      (error) => {
+        console.log(error.text);
+        Swal.fire({
+          icon: "error",
+          title: "Ooops, something went wrong",
+          text: error.text,
+        });
+      }
+    );
+    e.target.reset();
+  };
+
   return (
     <div className="contact_page">
       <h2>Contact</h2>
@@ -9,33 +38,59 @@ function Contact() {
         <div className="contact_question">
           <h2>Got a question? I'd love to hear from you</h2>
         </div>
-        <form className="contact_form">
-          <label for="firstname">First Name</label>
-          <input type="text" placeholder="First Name" />
-          <label for="lastname">Last Name</label>
-          <input type="text" placeholder="Last Name" />
-          <label for="email">email</label>
-          <input type="text" placeholder="email" />
-          <label for="country">Country</label>
-          <select for="country" placeholder="country">
-            <option value="Ireland">Ireland</option>
-            <option value="France">France</option>
-            <option value="UK">UK</option>
-            <option value="United States">United States</option>
-          </select>
-          <label for="subject">Subject</label>
+        <form className="contact_form" onSubmit={handleOnSubmit}>
+          <input type="text" placeholder="name" name="from_name" />
+          <input type="text" placeholder="email" name="from_email" />
           <textarea
             className="contact_textarea"
-            id="subject"
-            name="subject"
             placeholder="Write something"
-            maxlength="500"
+            maxLength="500"
+            name="from_message"
           ></textarea>
-          <input type="submit" value="Submit" />
+          <button type="submit" value="Submit">
+            Submit
+          </button>
         </form>
       </div>
     </div>
   );
+
+  // <div className="App">
+  //   <form onSubmit={handleOnSubmit}>
+  //     <input
+  //       id="form-input-control-email"
+  //       control={Input}
+  //       label="Email"
+  //       name="user_email"
+  //       placeholder="Email…"
+  //       required
+  //       icon="mail"
+  //       iconPosition="left"
+  //     />
+  //     <input
+  //       id="form-input-control-last-name"
+  //       control={Input}
+  //       label="Name"
+  //       name="user_name"
+  //       placeholder="Name…"
+  //       required
+  //       icon="user circle"
+  //       iconPosition="left"
+  //     />
+  //     <input
+  //       id="form-textarea-control-opinion"
+  //       control={TextArea}
+  //       label="Message"
+  //       name="user_message"
+  //       placeholder="Message…"
+  //       required
+  //     />
+  //     <button type="submit" color="green">
+  //       Submit
+  //     </button>
+  //   </form>
+  // </div>
+  // );
 }
 
 export default Contact;
