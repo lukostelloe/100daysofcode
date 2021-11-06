633120077485d44936bfeb1cf02b2f145957eb3f
lukostelloe <lukostelloe@gmail.com>
2021-11-06 19:52:58 +0100

    lists and ids

--- philosogram/philclient/src/components/Read/Read.js
+++ philosogram/philclient/src/components/Read/Read.js
@@ -4,6 +4,7 @@ function Read() {
   console.log(summaryModel);
   day22;
   // day23: changes made to axios in final project
+  //day24: arranging lists and user ids to assocaite certain items
 
   return <div>hi</div>;
 }
